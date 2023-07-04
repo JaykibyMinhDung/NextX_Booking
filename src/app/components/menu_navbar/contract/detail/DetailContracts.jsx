50435459faf0e1bb88d38f4aed56df816bdebb4f
JaykibyMinhDung <doanminhdung1729@gmail.com>
2023-07-04 09:26:20 +0700

    kích hoat thành công tailwind trên vite react

--- src/app/components/menu_navbar/contract/detail/DetailContracts.jsx
+++ src/app/components/menu_navbar/contract/detail/DetailContracts.jsx
@@ -3,6 +3,7 @@
 import { useLocation } from "react-router-dom";
 import TitlePage from "../../../../../styles/titlepage/TitlePage";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { useState } from "react";
 
 import "swiper/css";
 
@@ -14,20 +15,31 @@ const arrayHeader = [
 ];
 
 const DetailContracts = () => {
+  const [active, isActive] = useState(null);
   const location = useLocation();
   const informationHandle = () => {
     return location.state.data.find(
       (e) => e.prcode_text === location.state.prcode
     );
   };
-  const dataNavigate = informationHandle();
+  const activeHandle = (prop) => {
+    return isActive(prop);
+  };
+  // const dataNavigate =
+  informationHandle();
   return (
     <div>
       <TitlePage title={location.state.prcode} />
       <Swiper spaceBetween={10} slidesPerView={3}>
         {arrayHeader.map((e) => (
           <SwiperSlide key={e}>
-            <div style={{ width: "8rem", textAlign: "center" }}>{e}</div>
+            <div
+              onClick={() => activeHandle(e)}
+              className={active === e ? "border-b-blue-500 text-red-600" : ""}
+              style={{ width: "8rem", textAlign: "center" }}
+            >
+              {e}
+            </div>
           </SwiperSlide>
         ))}
       </Swiper>
